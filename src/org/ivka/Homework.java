d0ed29684ebb8283b10b01b2a38572d341ff1808
Ivka <ivka@dsoft-bg.com>
2020-04-08 21:55:01 +0300

    some more code

--- src/org/ivka/Homework.java
+++ src/org/ivka/Homework.java
@@ -3,5 +3,7 @@ package org.ivka;
 public class Homework {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("Some more code");
     }
 }
