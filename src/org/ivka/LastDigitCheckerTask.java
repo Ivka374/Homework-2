756c15d772dbc5f5b534e35a72e176d011ff77fb
Ivka <ivka@dsoft-bg.com>
2020-04-13 19:28:35 +0300

    Task9

---
+++ src/org/ivka/LastDigitCheckerTask.java
@@ -0,0 +1,26 @@
+package org.ivka;
+
+import java.util.Scanner;
+
+public class LastDigitCheckerTask {
+
+    public static boolean hasSameLastDigit(int a, int b, int c){
+        if (!isValid(a) || !isValid(b) || !isValid(c))return false;
+        else {
+            return a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10;
+        }
+    }
+
+    public static boolean isValid(int number){
+        return number >= 10 && number <= 1000;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(hasSameLastDigit(a, b, c));
+        System.out.println(isValid(scanner.nextInt()));
+    }
+}
