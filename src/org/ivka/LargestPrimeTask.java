0bf0753943aa8f839901d64b1213e3cc9fe9f921
Ivka <ivka@dsoft-bg.com>
2020-04-13 18:58:20 +0300

    Task8 - unfinished

--- src/org/ivka/LargestPrimeTask.java
+++ src/org/ivka/LargestPrimeTask.java
@@ -1,10 +1,33 @@
 package org.ivka;
 
 public class LargestPrimeTask {
+
     public static int getLargestPrime(int number){
         if (number <= 0)return -1;
         else {
-            return 0;
+            int maxPrime;
+            for (int i = 1; i <= number; i++){
+                if (isPrime(i)){
+                    do {
+                        if (number % 1 == 0){
+                            maxPrime = i;
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    public static boolean isPrime(int test){
+        int divident;
+        for (int i = 1; i < test; i++){
+            if (test % i == 0)divident = i;
+        }
+        if (divident == 1)return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
