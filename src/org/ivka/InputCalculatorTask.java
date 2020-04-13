2b39423418c911212961db400466daf37d11d427
Ivka <ivka@dsoft-bg.com>
2020-04-13 18:31:58 +0300

    Task7

--- src/org/ivka/InputCalculatorTask.java
+++ src/org/ivka/InputCalculatorTask.java
@@ -1,12 +1,31 @@
 package org.ivka;
 
+import java.util.Scanner;
+
 public class InputCalculatorTask {
 
     public static void inputThenPrintSumAndAverage(){
-
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        long average;
+        while(true){
+            if (scanner.hasNextInt()){
+                int number = scanner.nextInt();
+                sum += number;
+                count++;
+            } else {
+                if (count >= 1) {
+                    average = Math.round(sum / count);
+                } else average = 0;
+                break;
+            }
+        }
+        scanner.close();
+        System.out.println("SUM = " + sum + " AVG = " + average);
     }
 
     public static void main(String[] args) {
-
+        inputThenPrintSumAndAverage();
     }
 }
---
+++ src/org/ivka/LargestPrimeTask.java
@@ -0,0 +1,10 @@
+package org.ivka;
+
+public class LargestPrimeTask {
+    public static int getLargestPrime(int number){
+        if (number <= 0)return -1;
+        else {
+            return 0;
+        }
+    }
+}
