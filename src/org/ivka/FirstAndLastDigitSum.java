0df629b98fed8e62e06a6821c2cfece842c00726
Ivka <ivka@dsoft-bg.com>
2020-04-13 17:26:44 +0300

    small corrections

--- src/org/ivka/FirstAndLastDigitSum.java
+++ src/org/ivka/FirstAndLastDigitSum.java
@@ -1,12 +1,24 @@
 package org.ivka;
 
+import java.util.Scanner;
+
 public class FirstAndLastDigitSum {
 
     public static int sumFirstAndLastDigit(int number){
-
+        if (number >= 0) {
+            int sum = number % 10;
+            for (int i = number / 10; i > 0; i /= 10) {
+                if (i < 10) sum += i;
+            }
+            if (number < 10)sum *= 2;
+            return sum;
+        } else return -1;
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(sumFirstAndLastDigit(scanner.nextInt()));
 
+        scanner.close();
     }
 }
