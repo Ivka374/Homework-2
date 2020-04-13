d322ba7c6fa3c0e7079fe7cc42c5a2c5624b2bf2
Ivka <ivka@dsoft-bg.com>
2020-04-13 19:03:19 +0300

    unfinished

--- src/org/ivka/LargestPrimeTask.java
+++ src/org/ivka/LargestPrimeTask.java
@@ -5,26 +5,8 @@ public class LargestPrimeTask {
     public static int getLargestPrime(int number){
         if (number <= 0)return -1;
         else {
-            int maxPrime;
-            for (int i = 1; i <= number; i++){
-                if (isPrime(i)){
-                    do {
-                        if (number % 1 == 0){
-                            maxPrime = i;
-                        }
-                    }
-                }
-            }
+            return 0;
         }
-    }
-
-    public static boolean isPrime(int test){
-        int divident;
-        for (int i = 1; i < test; i++){
-            if (test % i == 0)divident = i;
-        }
-        if (divident == 1)return true;
-        else return false;
     }
 
     public static void main(String[] args) {
