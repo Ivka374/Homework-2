2f6485d3c9835b61ea4cec47bab8df408910a9fa
Ivka374 <ivka@dsoft-bg.com>
2020-04-15 20:41:50 +0300

    Last task

--- src/org/ivka/SharedDigitTask.java
+++ src/org/ivka/SharedDigitTask.java
@@ -1,8 +1,20 @@
 package org.ivka;
 
+import java.util.Scanner;
+
 public class SharedDigitTask {
 
-    public static void main(String[] args) {
+    public static boolean hasSharedDigit(int a, int b){
+        if (a < 10 || b < 10 || a > 99 || b > 90)return false;
+        else {
+            return a % 10 == b % 10 || a % 10 == b / 10 || a / 10 == b % 10 || a / 10 == b / 10;
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(hasSharedDigit(scanner.nextInt(), scanner.nextInt()));
+
+        scanner.close();
     }
 }
