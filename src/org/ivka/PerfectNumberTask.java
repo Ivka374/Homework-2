de0ba9e223b777b4245baba93000aaaf981f4655
Ivka374 <ivka@dsoft-bg.com>
2020-04-15 20:30:54 +0300

    Second to last task

--- src/org/ivka/PerfectNumberTask.java
+++ src/org/ivka/PerfectNumberTask.java
@@ -1,8 +1,24 @@
 package org.ivka;
 
+import java.util.Scanner;
+
 public class PerfectNumberTask {
 
-    public static void main(String[] args) {
+    public static boolean isPerfectNumber(int number){
+        if (number < 6)return false;
+        else {
+            int sumOfDividents = 0;
+            for (int i = 1; i < number; i++){
+                if (number%i == 0)sumOfDividents += i;
+            }
+            return sumOfDividents == number;
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(isPerfectNumber(scanner.nextInt()));
+
+        scanner.close();
     }
 }
